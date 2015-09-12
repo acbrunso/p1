e8119cb0c1f79a9b80433814a04e3acaf8c249b6
acbrunso <brunsonac@gmail.com>
2015-09-12 16:23:12 -0500

    udpate 2. css styles added.

--- index.php
+++ index.php
@@ -4,50 +4,55 @@
   <title>Landing Page P1</title>
   <meta charset='utf-8'>
   <link rel="stylesheet" type="text/css" href="style.css">
-
 </head>
 <body>
-  <h1>Anthony Brunson</h1>
-  <h1>Dynamic Web Apps Portfolio</h1>
-
-  <img src = "https://dl.dropboxusercontent.com/u/96607509/20150720_171545.jpg"
+  <div id="main">
+  <div id="header">
+    <h1>Anthony Brunson</h1>
+    <h1>Dynamic Web Apps Portfolio</h1>
+    <img src = "https://dl.dropboxusercontent.com/u/96607509/20150720_171545.jpg"
         alt = "headshot">
+  </div>
 
-  <h3>Project 2 Title</h3>
-  <a href="http://www.google.com">Placeholder</a>
-  <h3>Project 3 Title</h3>
-  <a href="http://www.google.com">Placeholder</a>
-  <h3>Project 4 Title</h3>
-  <a href="http://www.google.com">Placeholder</a>
-
-  <h2>In 2+ sentences describe who you are and what you do.</h2>
-  <p> My name is Anthony Brunson.  I am a First Lieutenant in the
-  United States Air Force and my job is Cyberspace Officer. I am
-  responsible for a $4.2B infrastructure that includes xxx million
-  mile fiber range, 21.1k personnel, and I oversee 121 personnel.
-  </p>
-  <h2>Why are you taking this course?</h2>
-  <p> I am taking this course because I intend on continuing in my current
-    field and I want to make the most out of my education as possible.
-    The Air Force has many different opportunities in the technology field
-    and holding a Master's Degree will give me the competitive edge I need
-    to progress.
-  </p>
-  <h2>What previous experience do you have with programming></h2>
-  <p>I have an Undergraduate Degree in Computer Science from Northern Illinois
-    Unversity. I have created several apps for class and have worked with databases
-    in the civilian sector and the military.
-  </p>
-  <h2>Are you working on a mac, PC, or some other platform?</h2>
-  <p> I am using a PC along with all of the recommended tools in the course.  I
-    prefer to use what is recommneded because the day that I stick to old ways
-    out of habit is the day I stop progressing.  It is important to be able to pick
-    up new/different technologies at any time and make it work.
-  </p>
-  <h2>Anything else we should know?</h2>
-  <p> I am married with a beautiful daughter, I run a rental business, and I work full time.
-    I am a busy person, but I always have time to further my knowledge in what I love, and I
-    love computers.
-  </p>
+  <div id="nav">
+    <h3>Project 2 Title</h3>
+      <a href="http://www.google.com">Placeholder</a>
+    <h3>Project 3 Title</h3>
+      <a href="http://www.google.com">Placeholder</a>
+    <h3>Project 4 Title</h3>
+      <a href="http://www.google.com">Placeholder</a>
+  </div>
+  <div id="section">
+    <h2>In 2+ sentences describe who you are and what you do.</h2>
+    <p> My name is Anthony Brunson.  I am a First Lieutenant in the
+      United States Air Force and my job is Cyberspace Officer. I am
+      responsible for a $4.2B infrastructure that includes xxx million
+      mile fiber range, 21.1k personnel, and I oversee 121 personnel.
+    </p>
+    <h2>Why are you taking this course?</h2>
+    <p> I am taking this course because I intend on continuing in my current
+      field and I want to make the most out of my education as possible.
+      The Air Force has many different opportunities in the technology field
+      and holding a Master's Degree will give me the competitive edge I need
+      to progress.
+    </p>
+    <h2>What previous experience do you have with programming?</h2>
+    <p>I have an Undergraduate Degree in Computer Science from Northern Illinois
+      Unversity. I have created several apps for class and have worked with databases
+      in the civilian sector and the military.
+    </p>
+    <h2>Are you working on a mac, PC, or some other platform?</h2>
+    <p> I am using a PC along with all of the recommended tools in the course.  I
+      prefer to use what is recommneded because the day that I stick to old ways
+      out of habit is the day I stop progressing.  It is important to be able to pick
+      up new/different technologies at any time and make it work.
+    </p>
+    <h2>Anything else we should know?</h2>
+    <p> I am married with a beautiful daughter, I run a rental business, and I work full time.
+      I am a busy person, but I always have time to further my knowledge in what I love, and I
+      love computers.
+    </p>
+  </div>
+</div>
 </body>
 </html>
